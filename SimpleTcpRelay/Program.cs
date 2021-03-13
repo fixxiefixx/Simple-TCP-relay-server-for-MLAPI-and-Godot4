a42e892df6aede65bb6f11aa375df536879f552b
fixx <fixx@hotmail.de>
2021-03-13 18:01:05 +0100

    Listening IP and port can now be configured via command line argument.

--- SimpleTcpRelay/Program.cs
+++ SimpleTcpRelay/Program.cs
@@ -6,6 +6,8 @@ namespace SimpleTcpRelay
 {
     public class Program
     {
+
+
         public static string ListenIPAddress = "0.0.0.0";
         public static int ListenPort = 8765;
 
@@ -13,10 +15,19 @@ namespace SimpleTcpRelay
         public static void Main(string[] args)
         {
             
+            if(args.Length>=1)
+            {
+                ListenIPAddress = args[0];
+            }
+            if(args.Length>=2)
+            {
+                ListenPort = int.Parse(args[1]);
+            }
+
             TcpListener listener = new TcpListener(IPAddress.Parse(ListenIPAddress), ListenPort);
             listener.Server.Blocking = true;
             listener.Start();
-            Console.WriteLine("Server started");
+            Console.WriteLine("Server started on "+ListenIPAddress+" port "+ListenPort);
             while(true)
             {
                 TcpClient client = listener.AcceptTcpClient();
